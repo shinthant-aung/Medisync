0514771e8b5609a9cd3ee60de016f17a14d5b635
KaungMyatThu30 <kgmyatthu10969@email.com>
2026-02-17 22:14:09 +0700

    .

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: "postgres", // Default user is usually 'postgres'
   host: "localhost",
   database: "Medisync", // The name we just gave our database
-  password: "Gen10@su", // <--- REPLACE THIS WITH YOUR PGADMIN PASSWORD
+  password: "password", // <--- REPLACE THIS WITH YOUR PGADMIN PASSWORD
   port: 5432, // Default PostgreSQL port
 });
 
